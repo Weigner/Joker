153cdd458f0f4e656587f4baac5014576be588ef
Erick Weigner <erick.weigner@boxdelivery.com.br>
2022-05-10 18:18:02 -0300

    implementando a tela About

--- app/src/main/java/com/weigner/joker/view/AboutFragment.kt
+++ app/src/main/java/com/weigner/joker/view/AboutFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.weigner.joker.BuildConfig
 import com.weigner.joker.R
 
 class AboutFragment : Fragment() {
@@ -15,5 +17,10 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.text_version).text = getString(R.string.build_version, BuildConfig.VERSION_NAME)
     }
 }
